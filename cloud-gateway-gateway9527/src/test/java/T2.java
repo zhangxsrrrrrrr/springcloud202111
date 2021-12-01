2b2189409719aa5f148032ec4d4cef18a8ec0836
zhangxun_a <zhangxun_a@si-tech.com.cn>
2021-12-01 16:41:24 +0800

    组件学习

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * 生成timeZone
+ *
+ * @author zhangxuna
+ * @date 2021-11-23 23:02
+ */
+public class T2 {
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
+    }
+}
